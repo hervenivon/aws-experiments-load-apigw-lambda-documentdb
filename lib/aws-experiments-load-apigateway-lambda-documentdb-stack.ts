d0d7bfc7633ad9f7219e1870d4a2ebfc9261c433
Hervé Nivon <nivonh@amazon.com>
2019-08-20 19:41:45 +0200

    Working example of API Gateway + Lambda

--- lib/aws-experiments-load-apigateway-lambda-documentdb-stack.ts
+++ lib/aws-experiments-load-apigateway-lambda-documentdb-stack.ts
@@ -1,18 +1,21 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
+import apigw = require('@aws-cdk/aws-apigateway');
 
 export class AwsExperimentsLoadApigatewayLambdaDocumentdbStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AwsExperimentsLoadApigatewayLambdaDocumentdbQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // defines an AWS lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_8_10,    // execution environment
+      code: lambda.Code.asset('lambda-node'), // code loaded from the "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    })
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
     });
-
-    const topic = new sns.Topic(this, 'AwsExperimentsLoadApigatewayLambdaDocumentdbTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
